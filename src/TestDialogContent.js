fb68c7b3df26bd6850763c4908a7a7cba23288a0
Jan Miksovsky <jmiksovsky@salesforce.com>
2021-02-25 15:48:16 -0800

    Experiments

--- src/TestDialogContent.js
+++ src/TestDialogContent.js
@@ -1,27 +1,64 @@
 import {
-  firstRender,
+  defaultState,
   ids,
   render,
+  rendered,
+  setState,
+  state,
   template,
 } from "../node_modules/elix/src/base/internal";
 import { templateFrom } from "../node_modules/elix/src/core/htmlLiterals";
 import ReactiveElement from "../node_modules/elix/src/core/ReactiveElement.js";
 
 class TestDialogContent extends ReactiveElement {
+  get [defaultState]() {
+    return Object.assign(super[defaultState], {
+      header: "",
+    });
+  }
+
+  get header() {
+    return this[state].header;
+  }
+  set header(header) {
+    this[setState]({ header });
+  }
+
   [render](changed) {
     super[render](changed);
-    if (this[firstRender]) {
-      this[ids].headerSlot.addEventListener("slotchange", () => {
-        const header = getHeaderText(this[ids].headerSlot);
-        this.dispatchEvent(
-          new CustomEvent("headerchange", {
-            bubbles: true,
-            detail: {
-              header,
-            },
-          })
-        );
-      });
+
+    // if (this[firstRender]) {
+    //   this[ids].headerSlot.addEventListener("slotchange", () => {
+    //     const header = getHeaderText(this[ids].headerSlot);
+    //     this.dispatchEvent(
+    //       new CustomEvent("headerchange", {
+    //         bubbles: true,
+    //         detail: {
+    //           header,
+    //         },
+    //       })
+    //     );
+    //   });
+    // }
+
+    if (changed.header) {
+      this[ids].header.textContent = this[state].header;
+    }
+  }
+
+  [rendered](changed) {
+    super[rendered](changed);
+
+    if (changed.header) {
+      const { header } = this[state];
+      this.dispatchEvent(
+        new CustomEvent("headerchange", {
+          bubbles: true,
+          detail: {
+            header,
+          },
+        })
+      );
     }
   }
 
@@ -32,7 +69,8 @@ class TestDialogContent extends ReactiveElement {
           padding: 1em;
         }
       </style>
-      <slot id="headerSlot" name="header"></slot>
+      <h1 id="header"></h1>
+      <!-- <slot id="headerSlot" name="header"></slot> -->
       <slot></slot>
     `;
   }
